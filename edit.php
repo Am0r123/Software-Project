4f599057b9e97f1ed905082b07ad565cb1c93fc9
Am0r123 <amrwael654@gmail.com>
2024-10-29 03:47:46 +0300

    fix

--- edit.php
+++ edit.php
@@ -51,7 +51,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./edit-user.css">
+    <link rel="stylesheet" href="./edit.css">
     <title>Edit User</title>
 </head>
 <body>
